10cfaec1eb83e1b34c9630b473150a372aca7e39
rcande65 <115851308+rcande65@users.noreply.github.com>
2023-05-05 20:44:17 -0700

    Delete project_benches/lc3 directory
    Remove Directory

--- project_benches/lc3/sim/regress.sh
+++
@@ -1,4 +0,0 @@
-rm *ucdb
-make cli TEST_SEED=11 BATCH_DO_COMMANDS='-do "set NoQuitOnFinish 1; run -a; coverage attribute -name TESTNAME -value $(TEST_NAME).$(TEST_SEED); coverage save $(TEST_NAME).$(TEST_SEED).ucdb; quit -f"'
-make run_cli TEST_SEED=211 BATCH_DO_COMMANDS='-do "set NoQuitOnFinish 1; run -a; coverage attribute -name TESTNAME -value $(TEST_NAME).$(TEST_SEED); coverage save $(TEST_NAME).$(TEST_SEED).ucdb; quit -f"'
-make merge_coverage
